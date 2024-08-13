e215ccd7233a04015591a3cc77a2f42263086919
KalyanG7 <71138039+KalyanG7@users.noreply.github.com>
2024-08-13 20:42:22 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.ai.embedding.EmbeddingClient;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@Configuration
+class AppConfig {
+	@Bean
+	VectorStore vectorStore(EmbeddingClient embedding) {
+		return new SimpleVectorStore(embedding);
+	}
+}
---
+++ src/main/java/com/example/demo/PdfFileReader.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import org.springframework.ai.reader.ExtractedTextFormatter;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@RestController
+public class PdfFileReader {
+    private final VectorStore vectorStore;
+
+    @Value("classpath:*.pdf")
+    private Resource[] pdfResource;
+
+    public PdfFileReader(VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
+    }
+
+    @PostConstruct
+    @GetMapping("/content/print")
+    public void init() {
+
+        var config = PdfDocumentReaderConfig.builder()
+                .withPageExtractedTextFormatter(
+                        new ExtractedTextFormatter.Builder()
+                                .build())
+                .build();
+        for (Resource resource : pdfResource) {
+            var pdfReader = new PagePdfDocumentReader(resource, config);
+            var textSplitter = new TokenTextSplitter();
+            vectorStore.accept(textSplitter.apply(pdfReader.get()));
+        }
+
+    }
+}
